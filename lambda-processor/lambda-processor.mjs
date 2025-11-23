bc9431a791b39208f4f1f64e18cbc6c19ba9b473
timmy-aws <timothylmiddleton@gmail.com>
2025-11-23 22:52:16 +1100

    first commit

---
+++ lambda-processor/lambda-processor.mjs
@@ -0,0 +1,18 @@
+export const lambdaHandler = async (event) => {
+    console.log('event ', event)
+  for (const message of event.Records) {
+    await processMessageAsync(message);
+  }
+  console.info("done");
+};
+
+async function processMessageAsync(message) {
+  try {
+    console.log(`Processed message ${message.body}`);
+    // TODO: Do interesting work based on the new message
+    await Promise.resolve(1); //Placeholder for actual async work
+  } catch (err) {
+    console.error("An error occurred");
+    throw err;
+  }
+}
